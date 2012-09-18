53f9a6647056fef570810f210cffabfbf37a4a3d
Harald Held <harald.held@gmail.com>
2012-09-18 19:47:33 +0200

    Replaced signal handling by non-posix functions.
    This does work under Linux as well, so it could be used for both platforms actually.

--- main.cpp
+++ main.cpp
@@ -19,37 +19,32 @@ int main(int argc, char *argv[])
     w.show();
 
     //configure app's reaction to OS signals
-//    struct sigaction act, oact;
-//    memset((void*)&act, 0, sizeof(struct sigaction));
-//    memset((void*)&oact, 0, sizeof(struct sigaction));
-//    act.sa_flags = 0;
-//    act.sa_handler = &signalHandler;
-//    sigaction(SIGINT, &act, &oact);
-//    sigaction(SIGKILL, &act, &oact);
-//    sigaction(SIGQUIT, &act, &oact);
-//    sigaction(SIGSTOP, &act, &oact);
-//    sigaction(SIGTERM, &act, &oact);
-//    sigaction(SIGSEGV, &act, &oact);
+    signal(SIGINT, signalHandler);
+    signal(SIGKILL, signalHandler);
+    signal(SIGQUIT, signalHandler);
+    signal(SIGSTOP, signalHandler);
+    signal(SIGTERM, signalHandler);
+    signal(SIGSEGV, signalHandler);
 
     return a.exec();
 }
 
-//void signalHandler(int signal)
-//{
-//    switch(signal)
-//    {
-//    case SIGINT: printf("Received signal SIGINT: "); break;
-//    case SIGKILL: printf("Received signal SIGKILL: "); break;
-//    case SIGQUIT: printf("Received signal SIGQUIT: "); break;
-//    case SIGSTOP: printf("Received signal SIGSTOP: "); break;
-//    case SIGTERM: printf("Received signal SIGTERM: "); break;
-//    case SIGSEGV: printf("Received signal SIGSEGV: "); break;
-//    }
+void signalHandler(int signal)
+{
+    switch(signal)
+    {
+    case SIGINT: printf("Received signal SIGINT: "); break;
+    case SIGKILL: printf("Received signal SIGKILL: "); break;
+    case SIGQUIT: printf("Received signal SIGQUIT: "); break;
+    case SIGSTOP: printf("Received signal SIGSTOP: "); break;
+    case SIGTERM: printf("Received signal SIGTERM: "); break;
+    case SIGSEGV: printf("Received signal SIGSEGV: "); break;
+    }
 
-//    printf("Still saving everything before we end the application.\n");
+    printf("Still saving everything before we end the application.\n");
 
-//    mw->SaveProjectXMLFile();
-//    mw->on_toolButton_stopWorking_clicked();
+    mw->SaveProjectXMLFile();
+    mw->on_toolButton_stopWorking_clicked();
 
-//    QApplication::quit();
-//}
+    QApplication::quit();
+}
